ebd4187e1a53fd5d24f521ed99a4d13c88d2604a
PedroAfonsoBraga <pedroavfb@gmail.com>
2023-08-21 10:13:49 -0300

    add: loginPage

--- smart-inspecs/src/components/loginForm/index.tsx
+++ smart-inspecs/src/components/loginForm/index.tsx
@@ -1,6 +1,9 @@
 import { TextField, Button, Stack } from "@mui/material";
 import { useForm } from "react-hook-form";
-import { StyledTitle } from "./style";
+import { StyledParagraph, StyledTitle } from "./style";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Link } from "react-router-dom";
+import * as yup from "yup";
 
 //alterar para interfaces
 type FormValues = {
@@ -8,12 +11,18 @@ type FormValues = {
   password: string;
 };
 
+const loginFormSchema = yup.object().shape({
+  email: yup.string().required("E-mail é obrigatório").email("E-mail inválido"),
+  password: yup.string().required("Senha é obrigatória"),
+});
+
 export const LoginForm = () => {
   const form = useForm<FormValues>({
     defaultValues: {
       email: "",
       password: "",
     },
+    resolver: yupResolver(loginFormSchema),
   });
 
   const { register, handleSubmit, formState } = form;
@@ -25,7 +34,7 @@ export const LoginForm = () => {
     console.log(data);
   });
 
-  const customButtonStyle = {
+  const customButtonPrimary = {
     backgroundColor: "#006400",
     color: "white",
     padding: "10px",
@@ -33,11 +42,19 @@ export const LoginForm = () => {
     fontSize: "24px",
   };
 
+  const customButtonSecundary = {
+    backgroundColor: "transparent",
+    color: "#006400",
+    padding: "10px",
+    fontWeight: "600",
+    fontSize: "24px",
+    border: "1px solid #006400",
+  };
   return (
     <>
       <form noValidate onSubmit={onSubmit}>
         <Stack spacing={2} width={"90%"} fontFamily={"Poppins"}>
-          <StyledTitle>Login </StyledTitle>
+          <StyledTitle>Login</StyledTitle>
           <TextField
             label="E-mail"
             type="email"
@@ -60,10 +77,22 @@ export const LoginForm = () => {
             type="submit"
             variant="contained"
             color="primary"
-            style={customButtonStyle}
+            style={customButtonPrimary}
           >
             Login
           </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            style={customButtonSecundary}
+          >
+            Cadastre-se
+          </Button>
+
+          <StyledParagraph>
+            esqueceu a senha? <Link to="/">clique aqui</Link>
+          </StyledParagraph>
         </Stack>
       </form>
     </>
