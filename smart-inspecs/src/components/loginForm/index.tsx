9b1da2ba1f218e04842932b15e531b48a801c69a
PedroAfonsoBraga <pedroavfb@gmail.com>
2023-08-22 16:29:50 -0300

    add: commit changes

--- smart-inspecs/src/components/loginForm/index.tsx
+++ smart-inspecs/src/components/loginForm/index.tsx
@@ -1,9 +1,6 @@
 import { TextField, Button, Stack } from "@mui/material";
 import { useForm } from "react-hook-form";
-import { StyledParagraph, StyledTitle } from "./style";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Link } from "react-router-dom";
-import * as yup from "yup";
+import { StyledTitle } from "./style";
 
 //alterar para interfaces
 type FormValues = {
@@ -11,18 +8,12 @@ type FormValues = {
   password: string;
 };
 
-const loginFormSchema = yup.object().shape({
-  email: yup.string().required("E-mail é obrigatório").email("E-mail inválido"),
-  password: yup.string().required("Senha é obrigatória"),
-});
-
 export const LoginForm = () => {
   const form = useForm<FormValues>({
     defaultValues: {
       email: "",
       password: "",
     },
-    resolver: yupResolver(loginFormSchema),
   });
 
   const { register, handleSubmit, formState } = form;
@@ -34,7 +25,7 @@ export const LoginForm = () => {
     console.log(data);
   });
 
-  const customButtonPrimary = {
+  const customButtonStyle = {
     backgroundColor: "#006400",
     color: "white",
     padding: "10px",
@@ -42,19 +33,11 @@ export const LoginForm = () => {
     fontSize: "24px",
   };
 
-  const customButtonSecundary = {
-    backgroundColor: "transparent",
-    color: "#006400",
-    padding: "10px",
-    fontWeight: "600",
-    fontSize: "24px",
-    border: "1px solid #006400",
-  };
   return (
     <>
       <form noValidate onSubmit={onSubmit}>
         <Stack spacing={2} width={"90%"} fontFamily={"Poppins"}>
-          <StyledTitle>Login</StyledTitle>
+          <StyledTitle>Login </StyledTitle>
           <TextField
             label="E-mail"
             type="email"
@@ -77,22 +60,10 @@ export const LoginForm = () => {
             type="submit"
             variant="contained"
             color="primary"
-            style={customButtonPrimary}
+            style={customButtonStyle}
           >
             Login
           </Button>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            style={customButtonSecundary}
-          >
-            Cadastre-se
-          </Button>
-
-          <StyledParagraph>
-            esqueceu a senha? <Link to="/">clique aqui</Link>
-          </StyledParagraph>
         </Stack>
       </form>
     </>
--- smart-inspecs/src/components/loginForm/style.ts
+++ smart-inspecs/src/components/loginForm/style.ts
@@ -1,22 +1,11 @@
 import { styled } from "styled-components";
 
 export const StyledTitle = styled.h2`
-    color: var(--primary-color);
-    text-align: center;
-    font-family: Poppins;
-    font-size: 2.25rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-`
-
-export const StyledParagraph = styled.p`
-    font-size: 1rem;
-    text-align: center;
-    margin-top: 1rem;
-
-    a{
-        color: var(--primary-color);
-        text-decoration: none;
-    }
+color: var(--primary-color);
+text-align: center;
+font-family: Poppins;
+font-size: 2.25rem;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
 `
