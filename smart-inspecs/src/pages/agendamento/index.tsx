e97709ece2ad909f24fe3997f9607d8366a9fea0
PedroAfonsoBraga <pedroavfb@gmail.com>
2023-10-09 14:23:41 -0300

    add: tela de agendamento

---
+++ smart-inspecs/src/pages/agendamento/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Agendamento from "../../components/home/agendamento";
+
+const AgendamentoPage = () => {
+  return <Agendamento />;
+};
+
+export default AgendamentoPage;
